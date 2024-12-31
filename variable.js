de458e1171133dca131626cfd10b51a20dd2fd4f
sushil thakur <sushilt2059@gmail.com>
2024-12-31 09:34:58 +0545

    push

---
+++ variable.js
@@ -0,0 +1,153 @@
+//let x= 2
+//x=3
+// console.log(x);
+// String
+// Number
+// Boolean
+// null
+// undefined
+// symbol
+// Object
+// bigint
+
+// let firstName = "John";
+// let lastName = "Doe";
+// let fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+// let x = 5;
+// console.log(typeof x);
+
+// let y = -Infinity;
+// console.log(y);
+// let user = null;
+// console.log(user);
+// let m;
+// console.log(m);
+
+// let laptopDetails = {
+//     brand: "Dell",
+//     model: "XPS 13",
+//     color: "Black",
+//     ram: "16GB",
+//     processor: "Intel Core i7",
+//     storage: "512GB SSD",
+// };
+// console.log(laptopDetails);
+
+// arthimetic operators
+// assignment operators
+// comparison operators
+// logical operators
+
+// console.log(5 + 5);
+// console.log(5 - 5);
+// console.log(5 * 5);
+// console.log(5 / 5);
+// console.log(5 % 5);
+// console.log(5 ** 5);
+// console.log(5 == 5);
+// console.log(5 != 5);
+// console.log(5 > 5);
+// console.log(5 < 5);
+// console.log(5 >= 5);
+// console.log(5 <= 5);
+// console.log(5 && 5);
+// console.log(5 || 5);
+// console.log(!5);
+// console.log(5 && 5 || 5);
+// console.log(5 && (5 || 5));
+// console.log(5 && 5 && 5);
+// console.log(5 || 5 || 5);
+// console.log(5 || 5 && 5);
+// console.log(5 && 5 || 5);
+// console.log(5 || 5 && 5);
+// console.log(5 || 5 && 5);
+
+// let num1 = 99;
+// let num2 = 50;
+// let res = num1 % num2;
+// console.log(res);
+
+// assignment operators
+// ? =, +=, -=, *=, /=, %=, **=
+
+
+// let a =5;
+// a= a + 7;
+// console.log(a);
+
+
+// let a =5;
+// a+= 7;
+// console.log(a);
+
+// let num =10;
+// num %= 3;
+// console.log(num);
+
+// let m =2;
+// m = m** 4;
+// console.log(m);
+
+// comparison operators
+// == equal to, != not equal to , !== strict not equal to
+//  
+// > greater than
+// < less than
+// >= greater than or equal to
+// <= less than or equal to
+// === strict equal to
+//
+
+// let user1 ='MINAL100';
+// let user2 = 'minal100';
+// console.log(user1 == user2);
+
+// let user1 ='MINAL100';
+// let user2 = 'minal100';
+// console.log(user1 === user2);
+
+
+// let m = 5;
+// let n = 9;
+// console.log(m != n);
+// console.log(m !== n);
+
+// console.log(5 > 3); 
+// console.log(5 < 3);
+// console.log(5 >= 3);
+// console.log(5 <= 3);
+// console.log(5 == 3);
+// console.log(5 != 3);
+
+// logical operators
+// && and
+// || or  ! not
+
+
+// let condition1 = false;
+// let condition2 = true;
+// console.log(condition1 || condition2);
+
+// let condition1 = false;
+// let condition2 = true;
+// console.log(condition1 && condition2);
+
+// let c1 = "nepali" === "nepali";//true
+// let c2 = 5>9;//false
+// let c3 = "10" === 10; //true
+
+// let result = c1 || c2 || c3;
+// console.log(result);
+
+
+//?AND(&&) operator
+//muntiply
+
+let c1 = true ;
+let c2 = false;
+let c3 = true;
+let c4 = false;
+let result = c1 && c2 && c3 && c4;
+console.log(result);
