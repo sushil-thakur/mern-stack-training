ff0d3af9279f2d4f81a64b17a0607ff359025c6a
sushil thakur <sushilt2059@gmail.com>
2025-01-01 17:34:45 +0545

    push

--- variable.js
+++ variable.js
@@ -1,5 +1,5 @@
-//let x= 2
-//x=3
+// let x= 2
+// x=3
 // console.log(x);
 // String
 // Number
@@ -92,13 +92,13 @@
 
 // comparison operators
 // == equal to, != not equal to , !== strict not equal to
-//  
+ 
 // > greater than
 // < less than
 // >= greater than or equal to
 // <= less than or equal to
 // === strict equal to
-//
+
 
 // let user1 ='MINAL100';
 // let user2 = 'minal100';
@@ -142,12 +142,208 @@
 // console.log(result);
 
 
-//?AND(&&) operator
-//muntiply
+// ?AND(&&) operator
+// muntiply
 
-let c1 = true ;
-let c2 = false;
-let c3 = true;
-let c4 = false;
-let result = c1 && c2 && c3 && c4;
-console.log(result);
+// let c1 = true ;
+// let c2 = false;
+// let c3 = true;
+// let c4 = false;
+// let result = c1 && c2 && c3 && c4;
+// console.log(result);
+
+// //? not 
+// const c1 =false;
+// console.log(!c1);
+
+
+// scope
+// let x=5;
+// {
+//     let x=10;
+//     console.log(x);
+// }
+
+// conditionls
+
+  
+
+
+// let x=8;
+// let condition = 15> 8;
+// if (condition){
+//     console.log("hello 8");
+// }
+
+
+// if esle
+
+// let age = 18;
+// if (age >= 18) {
+//     console.log("You are eligible to vote");
+// } else {
+//     console.log("You are not eligible to vote");
+// }  
+
+
+// const condition = 2**4>=17;
+// if (condition){
+//     console.log("hello");
+// }else{
+//     console.log("hi");
+// }
+
+
+// check if a number is even or odd
+
+// let num = 100;
+// if (num % 2 ===0){
+//     console.log("the number is even");
+// }else{
+//     console.log("the number is odd");
+// }
+
+// find the largest number among two numbers
+// let num1 = 100;
+// let num2 = 200;
+// if (num1 > num2){
+//     console.log("num1 is greater");
+// }else{
+//     console.log("num2 is greater");
+// }
+
+// if  ...else if ...else
+// determine whether a number is positive, negative or zero
+
+// let num =0;
+// if (num > 0){
+//     console.log("the number is positive");
+// }else if (num < 0){
+//     console.log("the number is negative");
+// }else{
+//     console.log("the number is zero");
+// }
+
+// find the largest number among three numbers
+
+// let num1 = 100;
+// let num2 = 200;
+// let num3 = 300;
+// if (num1 > num2 && num1 > num3){
+//     console.log("num1 is greater");
+// }else if (num2 > num1 && num2 > num3){
+//     console.log("num2 is greater");
+// }else{
+//     console.log("num3 is greater");
+// }
+
+// ? ternary
+// let num = 11;
+// if (num >10){
+//     console.log("the number is greater than 10");
+// }else{
+//     console.log("the number is less than 10");
+// }
+
+// ?syntax
+// condition ? trueEXpression : falseExpression
+
+// num >10? console.log("the number is greater than 10"): console.log("the number is less than 10");
+
+// console.log(num >10? "the number is greater than 10": "the number is less than 10");
+
+// let x= 55;
+// let remainder = x%2;
+// remainder === 0 ? console.log("the number is even"): console.log("the number is odd");
+
+// console.log(remainder === 0 ? "the number is even": "the number is odd");
+
+// //?switch
+// let dayNumber = 5;
+// switch (dayNumber) {
+//     case 1:
+//         console.log("Sunday");
+//         break;
+//     case 2:
+//         console.log("Monday");    
+//         break;            
+//     case 3:
+//         console.log("Tuesday");
+//         break;    
+//     case 4:
+//         console.log("Wednesday");    
+//         break;            
+//     case 5:
+//         console.log("Thursday");
+//         break;    
+//     case 6:
+//         console.log("Friday");    
+//         break;            
+//     case 7:
+//         console.log("Saturday");
+//         break;    
+//     default:
+//         console.log("Invalid day number");
+//         break;
+// }       
+
+// ?loop
+// run code multiple time
+// iteration
+// for
+// while
+// do while
+// syntax
+// for (initialization; condition; increment/decrement){
+//     code to be executed
+// }
+// for (let i = 0; i <= 5; i++) {
+//     console.log(i);
+// }
+
+// while loop
+// syntax
+// while (condition){
+//     code to be executed
+// }
+// let i = 0;
+// while (i <= 69) {
+//     console.log(i);
+//     i++;
+// }
+
+// ?WA for loop to print number from 10 to 20
+// for (let i = 10; i <= 20; i++) { 
+//     console.log(i);
+// }
+
+// ?WA for loop to print number from 50 to 20
+// for (let i = 50; i >= 20; i-1) { 
+//     console.log(i);
+// }
+
+
+
+//?while
+//syntax
+// while (condition){
+//     code to be executed
+// }
+let i = 0;
+while (i <= 69) {    
+    console.log(i);
+    i = i +1;
+}
+
+//?do while
+//syntax
+// do{
+//     code to be executed
+// }while (condition);
+
+
+// let i = 0;    
+// do{
+//     console.log(i);
+//     i++;
+// }while (i <= 19);    
