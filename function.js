ce04f56730f942a8f79db7931f26bd669a8aac8b
sushil thakur <sushilt2059@gmail.com>
2025-01-02 10:21:19 +0545

    push

---
+++ function.js
@@ -0,0 +1,12 @@
+//function
+//DRY = do not repeat yourself
+
+//syntax
+//function functionName(){
+//    code to be executed
+//}
+// we need to call the function
+function greet() {
+    console.log('Hello World');
+}
+greet();
