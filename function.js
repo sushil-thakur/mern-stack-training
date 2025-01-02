b78322eaf12cb7f5a341050550f49a1cdb5ade54
sushil thakur <sushilt2059@gmail.com>
2025-01-02 10:34:06 +0545

    push

--- function.js
+++ function.js
@@ -1,12 +1,31 @@
 //function
 //DRY = do not repeat yourself
-
+//? code reuse
+//? single point of control
 //syntax
 //function functionName(){
 //    code to be executed
 //}
 // we need to call the function
-function greet() {
-    console.log('Hello World');
+// function greet() {
+//     console.log('Hello World');
+// }
+// greet();
+
+// function getSum (x,y) {
+//     console.log(x + y);
+// }
+//get sum (2, 3)
+// getSum(2, 3);
+
+// //?WAF which determines provided number is even or odd
+ function evenOdd(x) {
+    if (x % 2 === 0) {
+        console.log('The number is even');
+    } else {
+        console.log('The number is odd');
+    }
 }
-greet();
+evenOdd(100);
+
+
