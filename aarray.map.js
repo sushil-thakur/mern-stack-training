fd250c4ef070bfbf74aa4c443dc327bd0186b8d2
sushil thakur <sushilt2059@gmail.com>
2025-01-05 09:28:22 +0545

    array

---
+++ aarray.map.js
@@ -0,0 +1,10 @@
+//array
+//?map, filter, find ,findindex, reduce, sort, every, some
+
+
+//?uses map
+const numList =[1,2,3,4,5,6,7,8,9,10];
+numList.map((item,index,array)=>{
+    console.log(item,index);
+    
+})
